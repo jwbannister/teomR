acc4ec10ecd56e380fe22dec3cf5bba14ab0ac1d
John Bannister <jbannister@airsci.com>
2016-01-18 09:06:04 -0800

    Output summary data for discussion with Kent.

--- scripts/do_twb2.R
+++ scripts/do_twb2.R
@@ -8,23 +8,15 @@ library(dplyr)
 library(ggplot2)
 load("./data-clean/teom_data.RData")
 
-teom_locs <- pair_teoms(filter(teom_locs, deployment!="Olancha", deployment!="North Beach"))
+teom_locs <- pair_teoms(filter(teom_locs, deployment!="Olancha", 
+                               deployment!="North Beach"))
 north_group <-c("T29-3", "T29-4")
 central_group <- c("T12-1")
 south_group <- c("T3SW", "T3SE", "T2-2", "T2-3", "T2-4", "T5-4")
 teom_locs <- rename(teom_locs, x=easting.utm, y=northing.utm)
-teom_data <- inner_join(teom_data, select(teom_locs, deployment.id, deployment, dca.group, position),
+teom_data <- inner_join(teom_data, select(teom_locs, deployment.id, deployment, 
+                                          dca.group, position),
                         by="deployment.id")
-
-p_north <- plot_owens_borders(dcas=north_group) +
-           geom_point(data=filter(teom_locs, dca.group=="north"), mapping=aes(x=x, y=y)) +
-           geom_label(data=filter(teom_locs, dca.group=="north"), mapping=aes(x=x, y=y, label=upwind.angle)) 
-p_central <- plot_owens_borders(dcas=central_group) +
-           geom_point(data=filter(teom_locs, dca.group=="central"), mapping=aes(x=x, y=y)) +
-           geom_label(data=filter(teom_locs, dca.group=="central"), mapping=aes(x=x, y=y, label=upwind.angle)) 
-p_south <- plot_owens_borders(dcas=south_group) +
-           geom_point(data=filter(teom_locs, dca.group=="south"), mapping=aes(x=x, y=y)) +
-           geom_label(data=filter(teom_locs, dca.group=="south"), mapping=aes(x=x, y=y, label=upwind.angle)) 
 
 events <- group_by(teom_data, deployment.id) %>%
           filter(wd.avg < (teom_locs[teom_locs$deployment.id==unique(deployment.id), ]$upwind.angle + 11.25) &
@@ -46,17 +38,46 @@ clean_events <- joined_events %>%
                        teom.dw = deployment.id.y) %>%
                 mutate(pm10.delta = pm10.dw - pm10.uw)
 
-summ <- clean_events %>% group_by(dca.group, dir) %>% summarize(mean.pm10.uw = mean(pm10.uw),
-                                                   mean.pm10.dw = mean(pm10.dw),
-                                                   mean.pm10.delta = mean(pm10.delta),
-                                                   n = length(pm10.uw))
+events_summary <- clean_events %>% group_by(dca.group, dir) %>% 
+  summarize(mean.pm10.uw = mean(pm10.uw), mean.pm10.dw = mean(pm10.dw), 
+            mean.pm10.delta = mean(pm10.delta), 
+            n = length(pm10.uw))
 
-p_nn <- ggplot(filter(clean_events, dca.group=="north", dir=="N"), aes(x=pm10.delta))+
-  geom_density()
-p_ns <- ggplot(filter(clean_events, dca.group=="north", dir=="S"), aes(x=pm10.delta))+
-  geom_density()
-p_sn <- ggplot(filter(clean_events, dca.group=="south", dir=="N"), aes(x=pm10.delta))+
-  geom_density()
-p_ss <- ggplot(filter(clean_events, dca.group=="south", dir=="S"), aes(x=pm10.delta))+
-  geom_density()
+# distributions of pm10 deltas for north and south dca group events (central dca 
+# group does not have enough events to make useful conculsions) 
+p_nn <- ggplot(filter(clean_events, dca.group=="north", dir=="N"), 
+               aes(x=pm10.delta)) + 
+        geom_density()
+p_ns <- ggplot(filter(clean_events, dca.group=="north", dir=="S"), 
+               aes(x=pm10.delta)) +
+        geom_density()
+p_sn <- ggplot(filter(clean_events, dca.group=="south", dir=="N"), 
+               aes(x=pm10.delta)) +
+        geom_density()
+p_ss <- ggplot(filter(clean_events, dca.group=="south", dir=="S"), 
+               aes(x=pm10.delta)) +
+        geom_density()
+
+# plots of relevant DCA groups with associated teoms and upwind angles
+p_north <- plot_owens_borders(dcas=north_group) +
+           geom_point(data=filter(teom_locs, dca.group=="north"), 
+                      mapping=aes(x=x, y=y)) +
+           geom_label(data=filter(teom_locs, dca.group=="north"), 
+                      mapping=aes(x=x, y=y, label=deployment)) 
+p_central <- plot_owens_borders(dcas=central_group) +
+           geom_point(data=filter(teom_locs, dca.group=="central"), 
+                      mapping=aes(x=x, y=y)) +
+           geom_label(data=filter(teom_locs, dca.group=="central"), 
+                      mapping=aes(x=x, y=y, label=deployment)) 
+p_south <- plot_owens_borders(dcas=south_group) +
+           geom_point(data=filter(teom_locs, dca.group=="south"), 
+                      mapping=aes(x=x, y=y)) +
+           geom_label(data=filter(teom_locs, dca.group=="south"), 
+                      mapping=aes(x=x, y=y, label=deployment)) 
+
+write.csv(events_summary, 
+          file="./output/twb2_paired_teoms_DEC_events_summary.csv")
+write.csv(clean_events,
+          file="./output/twb2_paired_teoms_DEC_events_data.csv")
+
 
