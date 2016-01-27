17e282455339dd0bd1485fa56f88f83da4085f17
John Bannister <jbannister@airsci.com>
2016-01-26 16:02:34 -0800

    Minor commit for transfer to GitHub.

--- scripts/do.R
+++ scripts/do.R
@@ -10,7 +10,7 @@ library(ggplot2)
 load("./data-clean/teom_data.RData")
 teom_locs <- pair_teoms(teom_locs)
 teom_locs <- assign_wind_angle(teom_locs)
-df1 <- inner_join(teom_data, select(teom_locs, deployment.id, 
+df1 <- inner_join(teom_data, dplyr::select(teom_locs, deployment.id, 
                                           dca.group, position),
                         by="deployment.id")
 
@@ -22,32 +22,20 @@ joined_events <- inner_join(filter(events, tag=="UW"),
 
 clean_events <- joined_events %>% 
                 group_by(data.id.x) %>%
-                select(datetime, dca.group,
+                dplyr::select(datetime, dca.group,
                        teom.uw=deployment.x, ws.uw=ws.x, wd.uw=wd.x, 
                        pm10.uw=pm10.avg.x, 
                        teom.dw=deployment.y, ws.dw=ws.y, wd.dw=wd.y, 
                        pm10.dw=pm10.avg.y) %>%
-                mutate(day=lubridate::day(datetime)) %>%
+                mutate(day=lubridate::day(datetime),
+                       ws.avg=mean(c(ws.uw, ws.dw))) %>%
                 filter(ws.uw!=0,  ws.dw!=0) %>%
                ungroup() 
 
-print(
-ggplot(filter(clean_events, dca.group=="north (T29)"), aes(wd.uw)) +
-  geom_histogram()
-)
-
-print(
-ggplot(filter(clean_events, dca.group=="central (T12)"), aes(wd.uw)) +
-  geom_histogram()
-)
-
-print(
-ggplot(filter(clean_events, dca.group=="south (T2 & T3)"), aes(wd.uw)) +
-  geom_histogram()
-)
-
 daily_summary <- clean_events %>% group_by(day, dca.group) %>%
-  summarize(daily.pm10.uw=sum(pm10.uw)/24, daily.pm10.dw=sum(pm10.dw)/24) %>%
+  summarize(daily.pm10.uw=sum(pm10.uw)/24, daily.pm10.dw=sum(pm10.dw)/24,
+# convert wind speed from mph to m/s
+            ws.avg.mps=mean(c(ws.uw, ws.dw))*.44704) %>%
   mutate(pm10.delta=daily.pm10.dw - daily.pm10.uw) %>%
   ungroup()
 
@@ -62,23 +50,25 @@ write.csv(clean_events,
 north_group <-c("T29-3", "T29-4")
 central_group <- c("T12-1")
 south_group <- c("T3SW", "T3SE", "T2-2", "T2-3", "T2-4", "T5-4")
+basemap <- brick("~/dropbox/gis/owens/owens_20151111_30prcnt.tif")
+maprgb <- RStoolbox::ggRGB(basemap)
 p_north <- plot_owens_borders(dcas=north_group) +
            geom_point(data=filter(teom_locs, dca.group=="north (T29)"), 
                       mapping=aes(x=x, y=y)) +
            geom_label(data=filter(teom_locs, dca.group=="north (T29)"), 
-                      mapping=aes(x=x, y=y, label=deployment)) 
-p_central <- plot_owens_borders(dcas=central_group) +
-           geom_point(data=filter(teom_locs, dca.group=="central (T12)"), 
-                      mapping=aes(x=x, y=y)) +
-           geom_label(data=filter(teom_locs, dca.group=="central (T12)"), 
-                      mapping=aes(x=x, y=y, label=deployment)) 
-p_south <- plot_owens_borders(dcas=south_group) +
-           geom_point(data=filter(teom_locs, dca.group=="south (T2/T3)"), 
-                      mapping=aes(x=x, y=y)) +
-           geom_label(data=filter(teom_locs, dca.group=="south (T2/T3)"), 
-                      mapping=aes(x=x, y=y, label=deployment)) 
+                      mapping=aes(x=x, y=y, label=deployment),
+                      nudge_x=125) 
 
-write.csv(clean_events,
-          file="./output/twb2_paired_teoms_DEC_events_data.csv")
+north_blank <- p_north + theme(axis.line=element_blank(),
+                               axis.text.x=element_blank(),
+                               axis.text.y=element_blank(),
+                               axis.ticks=element_blank(),
+                               axis.title.x=element_blank(),
+                               axis.title.y=element_blank(),
+                               legend.position="none",
+                               panel.background=element_blank(),
+                               panel.border=element_blank(),
+                               panel.grid.major=element_blank(),
+                               panel.grid.minor=element_blank(),
+                               plot.background=element_blank())
 
-test <- filter(teom_data, deployment.id==1849)
